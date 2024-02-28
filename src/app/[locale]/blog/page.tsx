fa7c856e9a83ef753e56e6462ceb02dcb3363df0
Yannick Simon <simon.yannickk@gmail.com>
2024-02-28 21:40:55 +0100

    fix: deploy issue

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,11 +1,7 @@
-"use client"
-
-import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 export default function Blog() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
     <div className="bg-white">
