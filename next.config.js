2b15588217babf59dd5139d594c37396004ac202
yasi0405 <simon.yannickk@gmail.com>
2026-06-04 13:12:48 +0200

    restart: Revert "Initial commit"

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const createNextIntlPlugin = require('next-intl/plugin');
-const withNextIntl = createNextIntlPlugin();
-const nextConfig = {
-}
-
-module.exports = withNextIntl(nextConfig);
